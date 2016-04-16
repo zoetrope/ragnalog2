2c78f298e5127f358369d3b001658b2c2e8bbaae
zoetrope <a.ikezoe@gmail.com>
2016-04-16 12:12:56 +0900

    add test for EmbulkYamlGenerator

--- ragnalog-node/src/main/scala/com/arielnetworks/ragnalog/port.adapter/embulk/EmbulkYamlGenerator.scala
+++ ragnalog-node/src/main/scala/com/arielnetworks/ragnalog/port.adapter/embulk/EmbulkYamlGenerator.scala
@@ -5,20 +5,11 @@ import java.net.URL
 
 import org.fusesource.scalate.TemplateEngine
 
-class EmbulkYamlGenerator {
+class EmbulkYamlGenerator(bindings: Map[String, Any]) {
 
   def generate(uri: URL): String = {
     val engine = new TemplateEngine
     engine.workingDirectory = new File("./tmp")
-
-    val bindings =
-      Map("name" -> "Scalate",
-        "config.name" -> "fuuuuu",
-        "config" -> Map("hoge" -> "Scala"))
-
-    val output = engine.layout(uri.getPath, bindings)
-//    println(output)
-
-    output
+    engine.layout(uri.getPath, bindings)
   }
 }
--- ragnalog-node/src/test/scala/com/arielnetworks/ragnalog/port.adapter/embulk/EmbulkYamlGeneratorSpec.scala
+++ ragnalog-node/src/test/scala/com/arielnetworks/ragnalog/port.adapter/embulk/EmbulkYamlGeneratorSpec.scala
@@ -9,20 +9,43 @@ import scala.io.Source
 class EmbulkYamlGeneratorSpec extends FunSpec with DiagrammedAssertions with CustomMatchers {
 
   describe("generate yaml") {
-    describe("grok") {
-      val generator = new EmbulkYamlGenerator
-      val uri = getClass.getClassLoader.getResource("template/grok.mustache")
-      assert(uri != null)
-      val output = generator.generate(uri)
-      val lines = Source.fromURL(getClass.getClassLoader.getResource("expected/grok.yml")).getLines().mkString(System.lineSeparator())
 
-      output should sameYaml (lines)
+    val bindings = Map[String, Any](
+      "input_file" -> "/tmp/ragnalog/sample.log",
+      "extra" -> "ap1",
+      "index_name" -> "ragnalog-test",
+      "grok.grok_pattern_files" -> List("/home/ragnalog/grok-patterns"),
+      "sar.sar_options" -> "-A",
+      "sar.sadf_paths" -> List(
+        ("9" , "/opt/sysstat-9.0.6/sadf"),
+        ("10" , "/opt/sysstat-10.0.5/sadf"),
+        ("11" , "/opt/sysstat-11.0.7/sadf")
+      ),
+      "sar.exclude_fields" -> List("interrupts.*"),
+      "elasticsearch.host" -> "localhost",
+      "elasticsearch.port" -> "9300",
+      "elasticsearch.cluster_name" -> "ragnalog2.elasticsearch"
+    )
+    val generator = new EmbulkYamlGenerator(bindings)
+
+    describe("grok") {
+      val uri = getClass.getClassLoader.getResource("template/grok.mustache")
+      val actual = generator.generate(uri)
+      val expected = Source.fromURL(getClass.getClassLoader.getResource("expected/grok.yml")).getLines().mkString(System.lineSeparator())
+      actual should sameYaml(expected)
     }
     describe("sar") {
-
+      val uri = getClass.getClassLoader.getResource("template/sar.mustache")
+      val actual = generator.generate(uri)
+      println(actual)
+      val expected = Source.fromURL(getClass.getClassLoader.getResource("expected/sar.yml")).getLines().mkString(System.lineSeparator())
+      actual should sameYaml(expected)
     }
     describe("csv") {
-
+      val uri = getClass.getClassLoader.getResource("template/csv.mustache")
+      val actual = generator.generate(uri)
+      val expected = Source.fromURL(getClass.getClassLoader.getResource("expected/csv.yml")).getLines().mkString(System.lineSeparator())
+      actual should sameYaml(expected)
     }
   }
 }
