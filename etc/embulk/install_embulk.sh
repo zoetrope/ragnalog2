c13804875ee30ca02ed199ecacd956d64831294f
zoetrope <a.ikezoe@gmail.com>
2016-04-19 18:54:02 +0900

    use embulk bundle

--- etc/embulk/install_embulk.sh
+++ etc/embulk/install_embulk.sh
@@ -5,3 +5,4 @@ rm -rf ./bin/
 curl --create-dirs -o ./bin/embulk -L "http://dl.embulk.org/embulk-latest.jar"
 chmod +x ./bin/embulk
 
+./bin/embulk bundle
--- etc/embulk/install_plugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# TODO: use bundler
-
-rm -rf ./plugins/
-
-./bin/embulk gem install embulk-parser-grok -i ./plugins
-./bin/embulk gem install embulk-output-elasticsearch -i ./plugins
-# ./bin/embulk gem install embulk-filter-insert -i ./plugins
