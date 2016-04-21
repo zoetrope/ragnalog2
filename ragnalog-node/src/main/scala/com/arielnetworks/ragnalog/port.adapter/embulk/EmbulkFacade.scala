1d8f546b65f159939d961afbcc3728916c75f021
zoetrope <a.ikezoe@gmail.com>
2016-04-22 01:00:23 +0900

    implement EmbulkFacade

--- ragnalog-node/src/main/scala/com/arielnetworks/ragnalog/port.adapter/embulk/EmbulkFacade.scala
+++ ragnalog-node/src/main/scala/com/arielnetworks/ragnalog/port.adapter/embulk/EmbulkFacade.scala
@@ -9,7 +9,11 @@ import com.arielnetworks.ragnalog.application.RegistrationResult
 
 import scala.util.{Failure, Success, Try}
 
+case class CommandResult(command: String, returnCode: Int, stdout: String, stderr: String)
+
 case class PluginInfo(name: String, versions: Seq[String])
+
+case class ProcessRuntimeException(message: String) extends Exception(message)
 
 class EmbulkFacade(config: EmbulkConfiguration) {
 
@@ -22,32 +26,44 @@ class EmbulkFacade(config: EmbulkConfiguration) {
 
   //TODO: Should this class have RegistrationResponse?
   def run(yaml: Path): Try[String] = {
+    executeProcess(s"$embulk run $yaml -b $bundleDir")
+      .map(res => res.returnCode match {
+        case 0 => res.stdout
+        case _ => throw new ProcessRuntimeException(res.stdout)
+      })
+  }
+
+  def plugins(): Try[Seq[PluginInfo]] = {
+    val pluginNamePattern = """^(embulk-\S+) \((.*)\)$""".r
+
+    executeProcess(s"$embulk gem list -b $bundleDir")
+      .map(res => res.returnCode match {
+        case 0 =>
+          res.stdout.split(System.lineSeparator()).collect({
+            case pluginNamePattern(name, versions) => PluginInfo(name, versions.split(",").map(_.trim).toList)
+          })
+        case _ => throw new ProcessRuntimeException(res.stdout)
+      })
+  }
+
+  private val bufferSize = 65535
+
+  private def executeProcess(command: String): Try[CommandResult] = {
     try {
       val stdout = new StringBuilder
       val stderr = new StringBuilder
 
-      val ret = Process(s"$embulk run $yaml -b $bundleDir") ! ProcessLogger(s=>stdout.append(s + "\n"), s => stderr.append(s + "\n"))
+      val ret = Process(command) ! ProcessLogger(
+        s => {
+          if (stdout.length + s.length < bufferSize) stdout.append(s + System.lineSeparator())
+          //TODO: logging
+        },
+        s => {
+          if (stderr.length + s.length < bufferSize) stderr.append(s + System.lineSeparator())
+          //TODO: logging
+        })
 
-      println(s"ret: $ret")
-      println("****************************")
-      println(s"stdout: ${stdout.toString}")
-      println("****************************")
-      println(s"stderr: ${stderr.toString}")
-      println("****************************")
-
-      Success(stdout.toString)
-    } catch {
-      case e: Throwable => Failure(e)
-    }
-  }
-
-  def plugins(): Try[Seq[PluginInfo]] = {
-    try {
-      val ret = Process(s"$embulk gem list -b $bundleDir") !!
-      val pluginNamePattern = """^(embulk-\S+) \((.*)\)$""".r
-      Success(ret.split(System.lineSeparator()).collect({
-        case pluginNamePattern(name, versions) => PluginInfo(name, versions.split(",").map(_.trim).toList)
-      }))
+      Success(CommandResult(command, ret, stdout.toString(), stderr.toString()))
     } catch {
       case e: Throwable => Failure(e)
     }
