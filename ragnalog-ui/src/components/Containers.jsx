65174d915031c42f7fad037f4d21fd9e048aa768
zoetrope <a.ikezoe@gmail.com>
2016-04-24 22:04:23 +0900

    add containers page

--- ragnalog-ui/src/components/Containers.jsx
+++ ragnalog-ui/src/components/Containers.jsx
@@ -1,41 +1,116 @@
 import React, {Component, PropTypes} from 'react';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import FontIcon from 'material-ui/FontIcon';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import * as Styles from 'material-ui/styles'
+import IconButton from 'material-ui/IconButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
-
+const style = {
+  margin: 12
+};
+const iconStyles = {
+  marginRight: 24
+};
 class Containers extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
   render() {
-    return <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHeaderColumn>ID</TableHeaderColumn>
-          <TableHeaderColumn>Name</TableHeaderColumn>
-          <TableHeaderColumn>Status</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableRowColumn>1</TableRowColumn>
-          <TableRowColumn>John Smith</TableRowColumn>
-          <TableRowColumn>Employed</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>2</TableRowColumn>
-          <TableRowColumn>Randal White</TableRowColumn>
-          <TableRowColumn>Unemployed</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>3</TableRowColumn>
-          <TableRowColumn>Stephanie Sanders</TableRowColumn>
-          <TableRowColumn>Employed</TableRowColumn>
-        </TableRow>
-        <TableRow>
-          <TableRowColumn>4</TableRowColumn>
-          <TableRowColumn>Steve Brown</TableRowColumn>
-          <TableRowColumn>Employed</TableRowColumn>
-        </TableRow>
-      </TableBody>
-    </Table>
+
+    const rightIconMenu = (
+      <IconMenu iconButtonElement={
+          <IconButton>
+            <MoreVertIcon color={Styles.colors.grey400} />
+          </IconButton>
+        }
+      >
+        <MenuItem primaryText="View"/>
+        <MenuItem primaryText="Edit"/>
+        <MenuItem primaryText="Delete"/>
+      </IconMenu>
+    );
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        disabled={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+
+    return <div>
+      <h2>Containers</h2>
+      <TextField hintText="Search"/>
+      <RaisedButton
+        label="Add Container" style={style}
+        icon={<FontIcon className="material-icons">add_circle</FontIcon>}
+        onTouchTap={this.handleOpen}
+      />
+      <Dialog
+        title="Dialog With Actions"
+        actions={actions}
+        modal={true}
+        open={this.state.open}
+      >
+        Only actions can close this dialog.
+      </Dialog>
+      <Table selectable={false}>
+        <TableHeader displaySelectAll={false}>
+          <TableRow>
+            <TableHeaderColumn>ID</TableHeaderColumn>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Status</TableHeaderColumn>
+            <TableHeaderColumn></TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false} showRowHover={true}>
+          <TableRow>
+            <TableRowColumn>1</TableRowColumn>
+            <TableRowColumn>John Smith</TableRowColumn>
+            <TableRowColumn>Employed</TableRowColumn>
+            <TableRowColumn>{rightIconMenu}</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>2</TableRowColumn>
+            <TableRowColumn>Randal White</TableRowColumn>
+            <TableRowColumn>Unemployed</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>3</TableRowColumn>
+            <TableRowColumn>Stephanie Sanders</TableRowColumn>
+            <TableRowColumn>Employed</TableRowColumn>
+          </TableRow>
+          <TableRow>
+            <TableRowColumn>4</TableRowColumn>
+            <TableRowColumn>Steve Brown</TableRowColumn>
+            <TableRowColumn>Employed</TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
   }
 }
 
