d1bc5f6ec036f81eec74959ebe39511d649ea914
zoetrope <a.ikezoe@gmail.com>
2016-05-07 17:06:21 +0900

    add handler

--- ragnalog-ui/src/components/logfiles/LogFileList.jsx
+++ ragnalog-ui/src/components/logfiles/LogFileList.jsx
@@ -10,65 +10,161 @@ import {
 } from "material-ui/Table";
 import TextField from 'material-ui/TextField';
 import Pagination from "./Pagination"
+import RaisedButton from "material-ui/RaisedButton";
+import FlatButton from "material-ui/FlatButton";
+import FontIcon from "material-ui/FontIcon";
+import Dialog from "material-ui/Dialog";
+
+const styles = {
+  button: {
+    margin: 12
+  },
+  rightButton: {
+    margin: 12,
+    float: 'right'
+  },
+  buttonGroup: {
+    margin: "0 20px"
+  }
+};
 
 class LogFileList extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      selectedRows: "none"
+      selectedRows: "none",
+      openBulkSetDialog: false,
+      filterValue: "",
+      logType: "",
+      extra: ""
     }
   }
 
   handleRowSelection = (selectedRows) => {
-
     //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/3734
     // I can not call setState in onRowSelection handler.
     // this.setState({
     //   selectedRows: selectedRows,
     // });
     this.state.selectedRows = selectedRows;
+  };
 
-    console.log("handleRowSelection", selectedRows);
+  shouldComponentUpdate(nextProps, nextState) {
+    //TODO: reconstruct the component tree.
+    // When the TextFile value is changed, the rendering speed of the table is too slow.
+    return this.state.filterValue === nextState.filterValue
+      && this.state.logType === nextState.logType
+      && this.state.extra === nextState.extra;
+  }
+
+  handleOpenBulkSetDialog = () => {
+    this.setState({openBulkSetDialog: true})
+  };
+  handleCloseBulkSetDialog = () => {
+    this.setState({openBulkSetDialog: false})
+  };
+
+  handleFilterValueChange = (e) => {
+    this.setState({filterValue: e.target.value});
+    e.stopPropagation();
+  };
+  handleLogTypeChange = (e) => {
+    this.setState({logType: e.target.value});
+    e.stopPropagation();
+  };
+  handleExtraChange = (e) => {
+    this.setState({extra: e.target.value});
+    e.stopPropagation();
   };
 
   render() {
 
     //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/2189
     // We should call event.stopPropagation on TableRow, if we use Tab Component.
-    return <Table selectable={true} multiSelectable={true} enableSelectAll={true}
-                  onRowSelection={this.handleRowSelection}>
-      <TableHeader displaySelectAll={true} adjustForCheckbox={true}>
-        <TableRow onChange={e=>e.stopPropagation()}>
-          <TableHeaderColumn>Archive Name</TableHeaderColumn>
-          <TableHeaderColumn>Log Name</TableHeaderColumn>
-          <TableHeaderColumn>Log Type</TableHeaderColumn>
-          <TableHeaderColumn>Extra</TableHeaderColumn>
-        </TableRow>
-      </TableHeader>
-      <TableBody displayRowCheckbox={true} showRowHover={true} deselectOnClickaway={false} preScanRows={false}>
-        {this.props.logFiles
-          .map(logFile => {
-            return <TableRow key={logFile.id} selectable={true} onChange={e=>e.stopPropagation()}>
-              <TableRowColumn>{logFile.archiveName}</TableRowColumn>
-              <TableRowColumn>{logFile.logName}</TableRowColumn>
-              <TableRowColumn>{logFile.logType}</TableRowColumn>
-              <TableRowColumn>
-                <TextField name={logFile.id}
-                           value={logFile.extra}
-                           onClick={e=>e.stopPropagation()}/>
-              </TableRowColumn>
-            </TableRow>
-          })}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableRowColumn colSpan="4">
-            <Pagination page={this.props.page} limit={this.props.limit} onPageChange={this.props.onPageChange} />
-          </TableRowColumn>
-        </TableRow>
-      </TableFooter>
-    </Table>
+    return <div>
+      <div style={styles.buttonGroup}>
+        <TextField
+          hintText="filter"
+          // value={this.state.filterValue}
+          onChange={this.handleFilterValueChange}
+        />
+        <FlatButton
+          label="Filter" style={styles.button}
+          onTouchTap={e => this.props.onApplyFilter(e, this.state.filterValue)}
+        />
+        <RaisedButton
+          label="Register" style={styles.rightButton}
+          primary={true}
+          icon={<FontIcon className="material-icons">add_circle</FontIcon>}
+          onTouchTap={this.props.onRegister}
+        />
+        <RaisedButton
+          label="Bulk set" style={styles.rightButton}
+          secondry={true}
+          icon={<FontIcon className="material-icons">edit</FontIcon>}
+          onTouchTap={this.handleOpenBulkSetDialog}
+        />
+        <Dialog
+          title="Bulk set to selected rows"
+          modal={false}
+          open={this.state.openBulkSetDialog}
+          onRequestClose={this.handleCloseBulkSetDialog}
+        >
+          <TextField
+            hintText="log type"
+            onChange={this.handleLogTypeChange}
+          />
+          <FlatButton
+            label="set"
+            onTouchTap={e => this.props.onSetLogType}
+            onTouchTap={e => this.props.onSetLogType(e, this.state.selectedRows, this.state.logType)}
+          />
+          <br/>
+          <TextField
+            hintText="extra"
+            onChange={this.handleExtraChange}
+          />
+          <FlatButton
+            label="set"
+            onTouchTap={e => this.props.onSetExtra(e, this.state.selectedRows, this.state.extra)}
+          />
+        </Dialog>
+      </div>
+      <Table selectable={true} multiSelectable={true} enableSelectAll={true}
+             onRowSelection={this.handleRowSelection}>
+        <TableHeader displaySelectAll={true} adjustForCheckbox={true}>
+          <TableRow onChange={e=>e.stopPropagation()}>
+            <TableHeaderColumn>Archive Name</TableHeaderColumn>
+            <TableHeaderColumn>Log Name</TableHeaderColumn>
+            <TableHeaderColumn>Log Type</TableHeaderColumn>
+            <TableHeaderColumn>Extra</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={true} showRowHover={true} deselectOnClickaway={false} preScanRows={false}>
+          {this.props.logFiles
+            .map(logFile => {
+              return <TableRow key={logFile.id} selectable={true} onChange={e=>e.stopPropagation()}>
+                <TableRowColumn>{logFile.archiveName}</TableRowColumn>
+                <TableRowColumn>{logFile.logName}</TableRowColumn>
+                <TableRowColumn>{logFile.logType}</TableRowColumn>
+                <TableRowColumn>
+                  <TextField name={logFile.id}
+                             value={logFile.extra}
+                             onClick={e=>e.stopPropagation()}/>
+                </TableRowColumn>
+              </TableRow>
+            })}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableRowColumn colSpan="4">
+              <Pagination page={this.props.page} limit={this.props.limit} onPageChange={this.props.onPageChange}/>
+            </TableRowColumn>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </div>
   }
 }
 
--- ragnalog-ui/src/components/logfiles/LogFileMain.jsx
+++ ragnalog-ui/src/components/logfiles/LogFileMain.jsx
@@ -4,32 +4,13 @@ import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import {Tabs, Tab} from "material-ui/Tabs";
 import LogFileList from "./LogFileList";
-import TextField from 'material-ui/TextField';
-import RaisedButton from "material-ui/RaisedButton";
-import FlatButton from "material-ui/FlatButton";
-import FontIcon from "material-ui/FontIcon";
-import Dialog from "material-ui/Dialog";
-
-const styles = {
-  button: {
-    margin: 12
-  },
-  rightButton: {
-    margin: 12,
-    float: 'right'
-  },
-  buttonGroup: {
-    margin: "0 20px"
-  }
-};
 
 class LogFileMain extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      tab: "Unregistered",
-      openBulkSetDialog: false
+      tab: "Unregistered"
     }
   }
 
@@ -38,7 +19,7 @@ class LogFileMain extends Component {
     this.props.params.containerId = "test";
     const testParam = "archiveId=2548922f3dfe82e4e86b2154c91d0a14&status=Unregistered";
     const searchParams = new URLSearchParams(testParam);
-    
+
     // const searchParams = new URLSearchParams(this.props.location.search.slice(1));
     console.log("Archives will mount", this.props.params, searchParams.toString());
     this.props.fetchLogFiles(this.props.params.containerId, searchParams.toString());
@@ -62,7 +43,16 @@ class LogFileMain extends Component {
   handleRegister = (logfile)=> {
   };
 
-  handleUnregister = (logfile)=> {
+  handleApplyFilter = (e, filterValue)=> {
+    console.log("apply filter: ", filterValue);
+  };
+
+  handleSetLogType = (e, selectedRows, logType)=> {
+    console.log("set log type: ", selectedRows, logType);
+  };
+
+  handleSetExtra = (e, selectedRows, extra)=> {
+    console.log("set extra: ", selectedRows, extra);
   };
 
   handleTabChange = (tab)=> {
@@ -86,13 +76,6 @@ class LogFileMain extends Component {
     this.props.fetchLogFiles(this.props.params.containerId, searchParams.toString());
   };
 
-  handleOpenBulkSetDialog = () => {
-    this.setState({openBulkSetDialog: true})
-  };
-  handleCloseBulkSetDialog = () => {
-    this.setState({openBulkSetDialog: false})
-  };
-
   render() {
     return <div>
       <Tabs
@@ -100,40 +83,12 @@ class LogFileMain extends Component {
         onChange={this.handleTabChange}
       >
         <Tab label="Unregistered" value="Unregistered">
-          <div style={styles.buttonGroup}>
-            <TextField hintText="filter"/>
-            <FlatButton
-              label="Filter" style={styles.button}
-            />
-            <RaisedButton
-              label="Register" style={styles.rightButton}
-              primary={true}
-              icon={<FontIcon className="material-icons">add_circle</FontIcon>}
-            />
-            <RaisedButton
-              label="Bulk set" style={styles.rightButton}
-              secondry={true}
-              icon={<FontIcon className="material-icons">edit</FontIcon>}
-              onTouchTap={this.handleOpenBulkSetDialog}
-            />
-            <Dialog
-              title="Bulk set to selected rows"
-              modal={false}
-              open={this.state.openBulkSetDialog}
-              onRequestClose={this.handleCloseBulkSetDialog}
-            >
-              <TextField hintText="log type"/>
-              <FlatButton label="set"/>
-              <br/>
-              <TextField hintText="extra"/>
-              <FlatButton label="set"/>
-            </Dialog>
-          </div>
-
           <LogFileList
             logFiles={this.props.logFiles}
             onRegister={this.handleRegister}
-            onUnregister={this.handleUnregister}
+            onSetLogType={this.handleSetLogType}
+            onSetExtra={this.handleSetExtra}
+            onApplyFilter={this.handleApplyFilter}
             onPageChange={this.handlePageChange}
             page={this.props.currentPage}
             limit={Math.ceil(this.props.totalCount/100) - 1}
