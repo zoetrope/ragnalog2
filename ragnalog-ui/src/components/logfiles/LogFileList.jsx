7a16427d8e3f1b2b69a19b6a125d6873a9111a39
zoetrope <a.ikezoe@gmail.com>
2016-05-07 17:48:27 +0900

    separate LogFileTable component from LogFileList

--- ragnalog-ui/src/components/logfiles/LogFileList.jsx
+++ ragnalog-ui/src/components/logfiles/LogFileList.jsx
@@ -1,19 +1,10 @@
 import React, {Component, PropTypes} from 'react';
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-  TableFooter
-} from "material-ui/Table";
 import TextField from 'material-ui/TextField';
-import Pagination from "./Pagination"
 import RaisedButton from "material-ui/RaisedButton";
 import FlatButton from "material-ui/FlatButton";
 import FontIcon from "material-ui/FontIcon";
 import Dialog from "material-ui/Dialog";
+import LogFileTable from "./LogFileTable";
 
 const styles = {
   button: {
@@ -33,29 +24,12 @@ class LogFileList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedRows: "none",
       openBulkSetDialog: false,
       filterValue: "",
       logType: "",
-      extra: ""
+      extra: "",
+      selectedRows: "none"
     }
-  }
-
-  handleRowSelection = (selectedRows) => {
-    //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/3734
-    // I can not call setState in onRowSelection handler.
-    // this.setState({
-    //   selectedRows: selectedRows,
-    // });
-    this.state.selectedRows = selectedRows;
-  };
-
-  shouldComponentUpdate(nextProps, nextState) {
-    //TODO: reconstruct the component tree.
-    // When the TextFile value is changed, the rendering speed of the table is too slow.
-    return this.state.filterValue === nextState.filterValue
-      && this.state.logType === nextState.logType
-      && this.state.extra === nextState.extra;
   }
 
   handleOpenBulkSetDialog = () => {
@@ -65,6 +39,8 @@ class LogFileList extends Component {
     this.setState({openBulkSetDialog: false});
   };
 
+  //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/2189
+  // We should call event.stopPropagation on TextField.onChange, if we use Tab Component.
   handleFilterValueChange = (e) => {
     this.setState({filterValue: e.target.value});
     e.stopPropagation();
@@ -77,11 +53,18 @@ class LogFileList extends Component {
     this.setState({extra: e.target.value});
     e.stopPropagation();
   };
+  
+  handleRowSelection = (selectedRows) => {
+    //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/3734
+    // When setState is called in onRowSelection handler, selected rows checkbox is cleared.
+    this.state.selectedRows = selectedRows;
+    // this.setState({
+    //   selectedRows: selectedRows
+    // });
+  };
 
   render() {
 
-    //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/2189
-    // We should call event.stopPropagation on TableRow, if we use Tab Component.
     return <div>
       <div style={styles.buttonGroup}>
         <TextField
@@ -131,39 +114,13 @@ class LogFileList extends Component {
           />
         </Dialog>
       </div>
-      <Table selectable={true} multiSelectable={true} enableSelectAll={true}
-             onRowSelection={this.handleRowSelection}>
-        <TableHeader displaySelectAll={true} adjustForCheckbox={true}>
-          <TableRow onChange={e=>e.stopPropagation()}>
-            <TableHeaderColumn>Archive Name</TableHeaderColumn>
-            <TableHeaderColumn>Log Name</TableHeaderColumn>
-            <TableHeaderColumn>Log Type</TableHeaderColumn>
-            <TableHeaderColumn>Extra</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={true} showRowHover={true} deselectOnClickaway={false} preScanRows={false}>
-          {this.props.logFiles
-            .map(logFile => {
-              return <TableRow key={logFile.id} selectable={true} onChange={e=>e.stopPropagation()}>
-                <TableRowColumn>{logFile.archiveName}</TableRowColumn>
-                <TableRowColumn>{logFile.logName}</TableRowColumn>
-                <TableRowColumn>{logFile.logType}</TableRowColumn>
-                <TableRowColumn>
-                  <TextField name={logFile.id}
-                             value={logFile.extra}
-                             onClick={e=>e.stopPropagation()}/>
-                </TableRowColumn>
-              </TableRow>
-            })}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableRowColumn colSpan="4">
-              <Pagination page={this.props.page} limit={this.props.limit} onPageChange={this.props.onPageChange}/>
-            </TableRowColumn>
-          </TableRow>
-        </TableFooter>
-      </Table>
+      <LogFileTable
+        logFiles={this.props.logFiles}
+        page={this.props.page}
+        limit={this.props.limit}
+        onPageChange={this.props.onPageChange}
+        onRowSelection={this.handleRowSelection}
+      />
     </div>
   }
 }
---
+++ ragnalog-ui/src/components/logfiles/LogFileTable.jsx
@@ -0,0 +1,64 @@
+import React, {Component, PropTypes} from 'react';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+  TableFooter
+} from "material-ui/Table";
+import TextField from 'material-ui/TextField';
+import Pagination from "./Pagination"
+
+class LogFileTable extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return this.props.logFiles !== nextProps.logFiles;
+  }
+
+  render() {
+
+    //CAUTION: material-ui's bug https://github.com/callemall/material-ui/issues/2189
+    // We should call event.stopPropagation on TableRow, if we use Tab Component.
+    return <Table selectable={true} multiSelectable={true} enableSelectAll={true}
+                  onRowSelection={this.props.onRowSelection}>
+      <TableHeader displaySelectAll={true} adjustForCheckbox={true}>
+        <TableRow onChange={e=>e.stopPropagation()}>
+          <TableHeaderColumn>Archive Name</TableHeaderColumn>
+          <TableHeaderColumn>Log Name</TableHeaderColumn>
+          <TableHeaderColumn>Log Type</TableHeaderColumn>
+          <TableHeaderColumn>Extra</TableHeaderColumn>
+        </TableRow>
+      </TableHeader>
+      <TableBody displayRowCheckbox={true} showRowHover={true} deselectOnClickaway={false} preScanRows={false}>
+        {this.props.logFiles
+          .map(logFile => {
+            return <TableRow key={logFile.id} selectable={true} onChange={e=>e.stopPropagation()}>
+              <TableRowColumn>{logFile.archiveName}</TableRowColumn>
+              <TableRowColumn>{logFile.logName}</TableRowColumn>
+              <TableRowColumn>{logFile.logType}</TableRowColumn>
+              <TableRowColumn>
+                <TextField name={logFile.id}
+                           value={logFile.extra}
+                           onClick={e=>e.stopPropagation()}/>
+              </TableRowColumn>
+            </TableRow>
+          })}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableRowColumn colSpan="4">
+            <Pagination page={this.props.page} limit={this.props.limit} onPageChange={this.props.onPageChange}/>
+          </TableRowColumn>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  }
+}
+
+export default LogFileTable;
